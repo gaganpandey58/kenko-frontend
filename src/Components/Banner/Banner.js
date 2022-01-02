82bea5ee4ea86804a496d22442c24df2d4212991
Gagan Pandey <shakyanischal@nayakoseli.com>
2022-01-02 22:52:51 +0545

    Banner update

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+import './Banner.css'
 function Banner() {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='banner-text'>
+                <div className='b-text'>
+                    <h1>Predict Your Disease</h1>
+                    <h3>Identify possible conditions and treatment related to your symptoms.</h3>
+                    <p>Tell us a bit about yourself and how you’re feeling for suggested next steps and common treatment plans.</p>
+                </div>
+            </div>
+            <div className='banner-image'>
+                <div className='b-img'>
+                    <img src="doc.png" alt="Doctor" height="300px" width="300px" />
+                </div>
+            </div>
         </div>
     );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 import './Header.css';
 import AccountLogin from '../AccountLogin/AccountLogin';
+import Banner from '../Banner/Banner';
 function Header() {
     return ( 
-        <div className="bar">
-            <div className="logo">
-                <text className="logo-text">kenko</text>
+        <div>
+            <div className="bar">
+                <div className="logo">
+                    <text className="logo-text">kenko</text>
+                </div>
+                <div className="diseaselabel">Disease Prediction System</div>
+                
+                <div className='AccountLogin'><AccountLogin/></div>
             </div>
-         <div className="diseaselabel">Disease Prediction System</div>
-            
-            <div className='AccountLogin'><AccountLogin/></div>
+
+            <div className='banner'>
+                <Banner />
+            </div>
         </div>
      );
 }
