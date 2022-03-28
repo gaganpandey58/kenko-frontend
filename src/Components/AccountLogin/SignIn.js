678392d7eb66d24cd24c3bedfa35269792a67f87
Gagan Pandey <shakyanischal@nayakoseli.com>
2022-03-28 14:59:49 +0545

    signup fixed

--- src/Components/AccountLogin/SignIn.js
+++ src/Components/AccountLogin/SignIn.js
@@ -5,6 +5,7 @@ import Input from './Input';
 import useForm from './useForm';
 import validate from './validateInfo';
 import kenko from '../images/kenko-png.png';
+import { Link } from 'react-router-dom';
 
 function SignIn(){
 
@@ -17,9 +18,9 @@ function SignIn(){
                         <div className="left-column">
                             <div className="left-wrapper">
                                 <img src={kenko} className='img-logo' />
-                                {/* <h6>Disease Prediction System</h6> */}
                             </div>
                         </div>
+
                         <div className="right-column">
                                 <div className="right-wrapper">
                                     <img className='img-logo' src={kenko} />
@@ -27,34 +28,41 @@ function SignIn(){
                             <div className="form-header">
                                 <h2 className="heading">Sign in to Kenko</h2>
                             </div>
-                            {/* {!viewSignInPhone ? ( */}
                             <div className="form-box">
                                 <form onSubmit={handleSubmit} className='input-field'>
-                                    <Input 
-                                        autoFocus
-                                        className="signin-input"
-                                        label="Username or Email Address"
-                                        id="username"
-                                        name="username"
-                                        type="text"
-                                        value={values.username}
-                                        onChange={handleChange}
-                                    />
-                                    {errors.username && <p className='signin-p'>{errors.username}</p>}
-                                    <Input 
-                                        className="signin-input"
-                                        label="Password"
-                                        id="password"
-                                        name="password"
-                                        type="password"
-                                        value={values.password}
-                                        onChange={handleChange}
-                                    />
-                                    {errors.password && <p className='signin-p'>{errors.password}</p>}
-                                    <button className="signin-button">Sign In</button>
+                                    <div>
+                                        <Input 
+                                            autoFocus
+                                            className="signin-input"
+                                            label="Username or Email Address"
+                                            id="username"
+                                            name="username"
+                                            type="text"
+                                            value={values.username}
+                                            onChange={handleChange}
+                                        />
+                                        {errors.username && <p className='signin-p'>{errors.username}</p>}
+                                        <Input 
+                                            className="signin-input"
+                                            label="Password"
+                                            id="password"
+                                            name="password"
+                                            type="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                        />
+                                        {errors.password && <p className='signin-p'>{errors.password}</p>}
+                                    </div>
+
+                                    <div>
+                                        <button className="signin-button">Sign In</button>
+                                    </div>
                                 </form>
                             </div>
-                            <h1>Hello</h1>
+
+                            <div className='new-account'>
+                                <Link to='/signup'> <a>Create a new account!</a> </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
